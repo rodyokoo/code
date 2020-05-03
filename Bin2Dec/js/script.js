4604ea26a43dc0f67114f6cfafcfc1962982f3c1
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-03 12:33:48 -0400

    bin2dec finished

--- Bin2Dec/js/script.js
+++ Bin2Dec/js/script.js
@@ -1,8 +1,22 @@
 window.addEventListener('load', start);
+var binary;
 function start() {
   console.log('página carregada');
-  var binary = document.querySelector('#binary');
-  binary.addEventListener('keyup', bin);
 }
-
-function bin(event) {}
+var number;
+function toNum() {
+  binary = document.getElementById('binary').value;
+  if (binary === '') {
+    return alert('please, enter a binary number!');
+  }
+  binary.split('').map((char) => {
+    if (char !== '0' && char !== '1') {
+      alert('please, enter a binary number!');
+      document.getElementById('binary').reset();
+      document.getElementById('binary').value = '';
+    }
+  });
+  number = document.querySelector('#numeric');
+  number.textContent = 'Your numeric number is: ' + parseInt(binary, 2);
+  console.log(parseInt(binary, 2));
+}
