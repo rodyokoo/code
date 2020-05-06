b1ca72c42205684654f5264a1c388efa7f314e2d
Rodrigo Yokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-06 16:12:49 -0400

    Add files via upload

---
+++ christimas-lights/js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', console.log('page loaded!'));
+
+function blink() {
+  document.getElementById('red').style.margin = '0px';
+  document.getElementById('red').style.padding = '20px';
+}
