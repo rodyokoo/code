526bff278af067816b4a67c4beea98f7a14936de
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-09 19:56:40 -0400

    color cycle

---
+++ color-cycle/js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', console.log('loaded page!'));
+/*for (var i = 0; i <= 255; i++) {
+  for (var j = 0; j <= 255; j++) {
+    for (var k = 0; k <= 255; k++) {
+      document.getElementById('body').style.color =
+        'rgb(' + i + ', ' + j + ', ' + k + ')';
+    }
+  }
+}*/
+
+document.getElementById('body').innerHTML = 'red';
