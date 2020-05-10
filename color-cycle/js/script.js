487f780fc614231e805239412f1032e441089a53
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-10 16:39:23 -0400

    backgroundColoer

--- color-cycle/js/script.js
+++ color-cycle/js/script.js
@@ -1,11 +1,19 @@
-window.addEventListener('load', console.log('loaded page!'));
-/*for (var i = 0; i <= 255; i++) {
-  for (var j = 0; j <= 255; j++) {
-    for (var k = 0; k <= 255; k++) {
-      document.getElementById('body').style.color =
-        'rgb(' + i + ', ' + j + ', ' + k + ')';
-    }
-  }
-}*/
+window.addEventListener('load', start);
+function start() {
+  change();
+}
 
-document.getElementById('body').innerHTML = 'red';
+function change() {
+  setInterval(color, 100);
+}
+
+var i = 0;
+var j = 0;
+function color() {
+  if (i < 255) {
+    document.getElementById('body').style.backgroundColor =
+      'rgb(' + i + ', 0, 0)';
+    i++;
+    console.log(i);
+  }
+}
