95e4aea027f36e778537df125f1f70873c76355b
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-11 19:20:58 -0400

    colors

--- color-cycle/js/script.js
+++ color-cycle/js/script.js
@@ -1,32 +1,7 @@
 window.addEventListener('load', start);
 function start() {
-  change();
+  console.log('page loaded!!');
 }
-
-function change() {
-  setInterval(color, 20);
-}
-
-var i = 0;
-var j = 0;
-var k = 0;
-function color() {
-  if (i < 255) {
-    document.getElementById('body').style.backgroundColor =
-      'rgb(' + i + ', 0, 0)';
-    i++;
-    console.log('i = ' + i);
-  }
-  if (i === 255 && j < 255) {
-    document.getElementById('body').style.backgroundColor =
-      'rgb(' + i + ', ' + j + ', 0)';
-    j++;
-    console.log('j = ' + j);
-  }
-  if (j === 255 && k < 255) {
-    document.getElementById('body').style.backgroundColor =
-      'rgb(' + i + ', ' + j + ', ' + k + ')';
-    k++;
-    console.log('k = ' + k);
-  }
+if (document.getElementById('red').value > 255) {
+  document.getElementById('red').innerHTML = '255';
 }
