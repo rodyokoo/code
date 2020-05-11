a4f682c80f6d236cbbbe92f635de2108a17b77ee
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-11 10:49:44 -0400

    Update script.js

--- color-cycle/js/script.js
+++ color-cycle/js/script.js
@@ -4,16 +4,29 @@ function start() {
 }
 
 function change() {
-  setInterval(color, 100);
+  setInterval(color, 20);
 }
 
 var i = 0;
 var j = 0;
+var k = 0;
 function color() {
   if (i < 255) {
     document.getElementById('body').style.backgroundColor =
       'rgb(' + i + ', 0, 0)';
     i++;
-    console.log(i);
+    console.log('i = ' + i);
+  }
+  if (i === 255 && j < 255) {
+    document.getElementById('body').style.backgroundColor =
+      'rgb(' + i + ', ' + j + ', 0)';
+    j++;
+    console.log('j = ' + j);
+  }
+  if (j === 255 && k < 255) {
+    document.getElementById('body').style.backgroundColor =
+      'rgb(' + i + ', ' + j + ', ' + k + ')';
+    k++;
+    console.log('k = ' + k);
   }
 }
