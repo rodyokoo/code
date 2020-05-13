ebc80e62ff72205fd7c36acfb45caf1ce38d8e15
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-12 20:53:22 -0400

    bad day

--- color-cycle/js/script.js
+++ color-cycle/js/script.js
@@ -2,6 +2,31 @@ window.addEventListener('load', start);
 function start() {
   console.log('page loaded!!');
 }
-if (document.getElementById('red').value > 255) {
-  document.getElementById('red').innerHTML = '255';
+
+function red() {
+  var red = document.getElementById('red').value;
+  /*if (red > 255) {
+    document.getElementById('red').value = 255;
+  } else if (isNaN(red) === true) {
+    document.getElementById('red').value = '  0';
+  }*/
+  console.log('red = ' + red);
+}
+
+function green() {
+  var green = parseInt(document.getElementById('green').value, 10);
+  if (green > 255) {
+    document.getElementById('green').value = 255;
+  } else if (isNaN(green) === true) {
+    document.getElementById('green').value = '  0';
+  }
+}
+
+function blue() {
+  var blue = parseInt(document.getElementById('blue').value, 10);
+  if (blue > 255) {
+    document.getElementById('blue').value = 255;
+  } else if (isNaN(blue) === true) {
+    document.getElementById('blue').value = '  0';
+  }
 }
