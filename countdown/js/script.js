d31964b0b40a11d01b78c11e864ad6594c04e95d
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-14 17:28:29 -0400

    countdown

---
+++ countdown/js/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', start);
+function start() {
+  console.log('page loaded!');
+}
+document.getElementById('startButton').addEventListener('click', function () {
+  let fut = document.getElementById('date').value;
+});
