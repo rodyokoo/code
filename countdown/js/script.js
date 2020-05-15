96b6bdb598ce6d2c72d8434791b71e61c017999b
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-15 17:16:44 -0400

    countdown baby steps

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -3,5 +3,11 @@ function start() {
   console.log('page loaded!');
 }
 document.getElementById('startButton').addEventListener('click', function () {
-  let fut = document.getElementById('date').value;
+  let futureDate = document.getElementById('date').value;
+  let arrDate = futureDate.split('-');
+  let futureYear = parseInt(arrDate[0], 10);
+  let futureMouth = parseInt(arrDate[1], 10);
+  let futureDay = parseInt(arrDate[2], 10);
+  console.log(futureYear + '-' + futureMouth + '-' + futureDay);
+  console.log('|' + futureDate + '|');
 });
