72ebef5e63c83dd40e6952326475baabab294129
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-15 22:12:48 -0400

    leap year

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -9,5 +9,19 @@ document.getElementById('startButton').addEventListener('click', function () {
   let futureMouth = parseInt(arrDate[1], 10);
   let futureDay = parseInt(arrDate[2], 10);
   console.log(futureYear + '-' + futureMouth + '-' + futureDay);
-  console.log('|' + futureDate + '|');
+  year(futureYear, futureMouth, futureDay);
 });
+
+function year(year, mouth, day) {
+  let yearNum =
+    year * 365 + year / 4 - (Math.trunc(year / 100) - Math.trunc(year / 400));
+  let mouthNum;
+  if (mouth === 1) {
+    mouthNum = 0;
+  } else if (mouth === 2) {
+    mouthNum = 31;
+  }
+  console.log(yearNum);
+  console.log(mouthNum);
+  console.log(yearNum + mouthNum);
+}
