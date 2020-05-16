4569243b3425df9b7e69ad80a05ccd8de220f92c
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-16 19:29:02 -0400

    update evening

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -9,29 +9,54 @@ document.getElementById('startButton').addEventListener('click', function () {
   let futureMouth = parseInt(arrDate[1], 10);
   let futureDay = parseInt(arrDate[2], 10);
   console.log(futureYear + '-' + futureMouth + '-' + futureDay);
-  year(futureYear, futureMouth, futureDay);
+  future(futureYear, futureMouth, futureDay);
 });
 
-function year(year, mouth, day) {
+function future(year, mouth, day) {
   let yearNum =
-    year * 365 + year / 4 - (Math.trunc(year / 100) - Math.trunc(year / 400));
+    year * 365 +
+    Math.trunc((year - 1) / 4) -
+    (Math.trunc((year - 1) / 100) - Math.trunc((year - 1) / 400));
+
   let mouthNum;
   if (mouth === 1) {
     mouthNum = 0;
   } else if (mouth === 2) {
     mouthNum = 31;
-  } else if (mouth === 3){
-    if (year % 4 === 0){
-      if (year % 400 !== 0){
-        mouthNum = 59;
-      } else {
-        mouthNum = 60;
-      }
-    } else {
-      mouthNum = 59;
+  } else if (mouth === 3) {
+    mouthNum = 31 + 28;
+  } else if (mouth === 4) {
+    mouthNum = 31 + 28 + 31;
+  } else if (mouth === 5) {
+    mouthNum = 31 + 28 + 31 + 30;
+  } else if (mouth === 6) {
+    mouthNum = 31 + 28 + 31 + 30 + 31;
+  } else if (mouth === 7) {
+    mouthNum = 31 + 28 + 31 + 30 + 31 + 30;
+  } else if (mouth === 8) {
+    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31;
+  } else if (mouth === 9) {
+    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+  } else if (mouth === 10) {
+    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+  } else if (mouth === 11) {
+    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+  } else if (mouth === 12) {
+    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+  }
+
+  if (mouth > 2) {
+    if (year % 400 === 0) {
+      mouthNum = mouthNum + 1;
     }
-  } else if (mouth === )
+    if (year % 100 !== 0 && year % 4 === 0) {
+      mouthNum = mouthNum + 1;
+    }
+  }
+  let dayNum = day;
+
   console.log(yearNum);
   console.log(mouthNum);
-  console.log(yearNum + mouthNum);
+  console.log(dayNum);
+  console.log(yearNum + mouthNum + dayNum);
 }
