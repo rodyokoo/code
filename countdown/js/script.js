d9201bc3be148a7a311ad1b1881047dd5a789984
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-17 15:49:42 -0400

    sunday update

--- countdown/js/script.js
+++ countdown/js/script.js
@@ -6,57 +6,57 @@ document.getElementById('startButton').addEventListener('click', function () {
   let futureDate = document.getElementById('date').value;
   let arrDate = futureDate.split('-');
   let futureYear = parseInt(arrDate[0], 10);
-  let futureMouth = parseInt(arrDate[1], 10);
+  let futureMonth = parseInt(arrDate[1], 10);
   let futureDay = parseInt(arrDate[2], 10);
-  console.log(futureYear + '-' + futureMouth + '-' + futureDay);
-  future(futureYear, futureMouth, futureDay);
+  console.log(futureYear + '-' + futureMonth + '-' + futureDay);
+  let today = new Date();
+  let todayDate = [today.getFullYear(), today.getMonth() + 1, today.getDate()];
+  let future = totalDays(futureYear, futureMonth, futureDay);
+  let present = totalDays(todayDate[0], todayDate[1], todayDate[2]);
+  console.log(future - present + ' day(s) to your dead line');
 });
 
-function future(year, mouth, day) {
+function totalDays(year, month, day) {
   let yearNum =
     year * 365 +
     Math.trunc((year - 1) / 4) -
     (Math.trunc((year - 1) / 100) - Math.trunc((year - 1) / 400));
 
-  let mouthNum;
-  if (mouth === 1) {
-    mouthNum = 0;
-  } else if (mouth === 2) {
-    mouthNum = 31;
-  } else if (mouth === 3) {
-    mouthNum = 31 + 28;
-  } else if (mouth === 4) {
-    mouthNum = 31 + 28 + 31;
-  } else if (mouth === 5) {
-    mouthNum = 31 + 28 + 31 + 30;
-  } else if (mouth === 6) {
-    mouthNum = 31 + 28 + 31 + 30 + 31;
-  } else if (mouth === 7) {
-    mouthNum = 31 + 28 + 31 + 30 + 31 + 30;
-  } else if (mouth === 8) {
-    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31;
-  } else if (mouth === 9) {
-    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-  } else if (mouth === 10) {
-    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-  } else if (mouth === 11) {
-    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-  } else if (mouth === 12) {
-    mouthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+  let monthNum;
+  if (month === 1) {
+    monthNum = 0;
+  } else if (month === 2) {
+    monthNum = 31;
+  } else if (month === 3) {
+    monthNum = 31 + 28;
+  } else if (month === 4) {
+    monthNum = 31 + 28 + 31;
+  } else if (month === 5) {
+    monthNum = 31 + 28 + 31 + 30;
+  } else if (month === 6) {
+    monthNum = 31 + 28 + 31 + 30 + 31;
+  } else if (month === 7) {
+    monthNum = 31 + 28 + 31 + 30 + 31 + 30;
+  } else if (month === 8) {
+    monthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31;
+  } else if (month === 9) {
+    monthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+  } else if (month === 10) {
+    monthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+  } else if (month === 11) {
+    monthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+  } else if (month === 12) {
+    monthNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
   }
 
-  if (mouth > 2) {
+  if (month > 2) {
     if (year % 400 === 0) {
-      mouthNum = mouthNum + 1;
+      monthNum = monthNum + 1;
     }
     if (year % 100 !== 0 && year % 4 === 0) {
-      mouthNum = mouthNum + 1;
+      monthNum = monthNum + 1;
     }
   }
   let dayNum = day;
-
-  console.log(yearNum);
-  console.log(mouthNum);
-  console.log(dayNum);
-  console.log(yearNum + mouthNum + dayNum);
+  return yearNum + monthNum + dayNum;
 }
