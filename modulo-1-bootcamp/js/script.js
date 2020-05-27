ee364048d3913aca4f2dc28266c3e91d6ca392de
Rodrigo Yokoo <39280721+rodyokoo@users.noreply.github.com>
2020-05-27 16:21:39 -0300

    update

--- modulo-1-bootcamp/js/script.js
+++ modulo-1-bootcamp/js/script.js
@@ -1,1 +1,29 @@
-window.addEventListener('load', () => console.log('page loaded'));
+window.addEventListener('load', () => {
+  console.log('page loaded');
+  searchResult();
+});
+
+let person;
+
+function searchText() {
+  person = document.querySelector('#buscaTexto').value.toLowerCase();
+  searchResult();
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode !== 13) return;
+  searchText();
+});
+
+let personResult;
+
+function searchResult() {
+  let firstResult = people.results.filter(
+    (personSearch) => personSearch.name.first.toLowerCase() == person
+  );
+  let lastResult = people.results.filter(
+    (personSearch) => personSearch.name.last.toLowerCase() == person
+  );
+  personResult = [...firstResult, ...lastResult];
+  console.log(personResult);
+}
