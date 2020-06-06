6dd44992bf597a87c55d5160da04a1d5de6bd039
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-06-06 10:13:27 -0400

    modulo02

--- modulo-02-bootcamp/my-bank-api/index.js
+++ modulo-02-bootcamp/my-bank-api/index.js
@@ -1,30 +1,35 @@
 var express = require('express');
-var fs = require('fs');
+var fs = require('fs').promises;
 var app = express();
 var accountsRouter = require('./routes/accounts.js');
+const winston = require('winston');
 
 global.fileName = 'accounts.json';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'my-bank-api.log' }),
+  ],
+  format: combine(label({ label: 'my-bank-api' }), timestamp(), myFormat),
+});
 
 app.use(express.json());
 app.use('/account', accountsRouter);
 
-app.listen(3000, function () {
+app.listen(3000, async () => {
   try {
-    fs.readFile(global.fileName, 'utf8', (err, data) => {
-      if (err) {
-        const initialJson = {
-          nextId: 1,
-          accounts: [],
-        };
-        fs.writeFile(global.fileName, JSON.stringify(initialJson), (err) => {
-          if (err) {
-            console.log(err);
-          }
-        });
-      }
-    });
+    await fs.readFile(global.fileName, 'utf8');
+    logger.info('API started!');
   } catch (err) {
-    console.log(err);
+    const initialJson = { nextId: 1, accounts: [] };
+    fs.writeFile(global.fileName, JSON.stringify(initialJson)).catch((err) => {
+      logger.error(err);
+    });
   }
-  console.log('API started!');
 });
--- modulo-02-bootcamp/my-bank-api/routes/accounts.js
+++ modulo-02-bootcamp/my-bank-api/routes/accounts.js
@@ -1,79 +1,110 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+var fs = require('fs').promises;
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   let account = req.body;
-  fs.readFile(global.fileName, 'utf8', (err, data) => {
-    try {
-      if (err) throw err;
-      let json = JSON.parse(data);
-      account = { id: json.nextId++, ...account };
-      json.accounts.push(account);
-      fs.writeFile(global.fileName, JSON.stringify(json), (err) => {
-        if (err) {
-          res.status(400).send({ error: err.message });
-        } else {
-          res.end();
-        }
-      });
-    } catch (err) {
-      res.status(400).send({ error: err.message });
-    }
-  });
+  try {
+    let data = await fs.readFile(global.fileName, 'utf8');
+    let json = JSON.parse(data);
+
+    account = { id: json.nextId++, ...account };
+    json.accounts.push(account);
+
+    await fs.writeFile(global.fileName, JSON.stringify(json));
+    res.end();
+    logger.info(`POST /account - ${JSON.stringify(account)}`);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    logger.error(`POST /account - ${err.message}`);
+  }
 });
 
-router.get('/', (_, res) => {
-  fs.readFile(global.fileName, 'utf8', (err, data) => {
-    try {
-      if (err) throw err;
-      let json = JSON.parse(data);
-      delete json.nextId;
-      res.send(json);
-    } catch (err) {
-      res.status(400).send({ error: err.message });
-    }
-  });
+router.get('/', async (_, res) => {
+  try {
+    let data = await fs.readFile(global.fileName, 'utf8');
+    let json = JSON.parse(data);
+    delete json.nextId;
+    res.send(json);
+    logger.send('GET /account');
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    logger.error(`GET /account - ${err.message}`);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  fs.readFile(global.fileName, 'utf8', (err, data) => {
-    try {
-      if (err) throw err;
-      let json = JSON.parse(data);
-      const account = json.accounts.find(
-        (account) => account.id === parseInt(req.params.id, 10)
-      );
-      if (account) {
-        res.send(account);
-      } else {
-        res.end();
-      }
-    } catch (err) {
-      res.status(400).send({ error: err.message });
+router.get('/:id', async (req, res) => {
+  try {
+    let data = await fs.readFile(global.fileName, 'utf8');
+    let json = JSON.parse(data);
+    const account = json.accounts.find(
+      (account) => account.id === parseInt(req.params.id, 10)
+    );
+    if (account) {
+      res.send(account);
+      logger.info(`GET /account/:id - ${JSON.stringify(account)}`);
+    } else {
+      res.end();
+      logger.info('GET /account/:id');
     }
-  });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    logger.error(`GET /account/:id - ${err.message}`);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  fs.readFile(global.fileName, 'utf8', (err, data) => {
-    try {
-      if (err) throw err;
-      let json = JSON.parse(data);
-      let accounts = json.accounts.filter(
-        (account) => account.id !== parseInt(req.params.id, 10)
-      );
-      json.accounts = accounts;
-      fs.writeFile(global.fileName, JSON.stringify(json), (err) => {
-        if (err) {
-          res.status(400).send({ error: err.message });
-        } else {
-          res.end();
-        }
-      });
-    } catch (err) {
-      res.status(400).send({ error: err.message });
+router.delete('/:id', async (req, res) => {
+  try {
+    let data = await fs.readFile(global.fileName, 'utf8');
+    let json = JSON.parse(data);
+    let accounts = json.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id, 10)
+    );
+    json.accounts = accounts;
+    await fs.writeFile(global.fileName, JSON.stringify(json));
+    res.end();
+    logger.info(`DELETE /account/:id - ${req.params.id}`);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    logger.error(`DELETE /account - ${err.message}`);
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    let newAccount = req.body;
+    let data = await fs.readFile(global.fileName, 'utf8');
+    let json = JSON.parse(data);
+    let oldIndex = json.accounts.findIndex(
+      (account) => account.id === newAccount.id
+    );
+    json.accounts[oldIndex].name = newAccount.name;
+    json.accounts[oldIndex].balance = newAccount.balance;
+    await fs.writeFile(global.fileName, JSON.stringify(json));
+    res.end();
+    logger.info(`PUT /account - ${JSON.stringify(newAccount)}`);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    logger.error(`PUT /account - ${err.message}`);
+  }
+});
+
+router.post('/transaction', async (req, res) => {
+  try {
+    let params = req.body;
+    let data = await fs.readFile(global.fileName, 'utf8');
+    let json = JSON.parse(data);
+    let index = json.accounts.findIndex((account) => account.id === params.id);
+    if (params.value < 0 && json.accounts[index].balance + params.value < 0) {
+      throw new Error('Não há saldo suficiente');
     }
-  });
+    json.accounts[index].balance += params.value;
+    await fs.writeFile(global.fileName, JSON.stringify(json));
+    res.send(json.accounts[index]);
+    logger.info(`POST /account/transaction - ${JSON.stringify(params)}`);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    logger.error(`POST /account/transaction - ${err.message}`);
+  }
 });
 module.exports = router;
