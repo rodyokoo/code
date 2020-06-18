fbe523e3cce5d893c3c6267a3a4e9f8b6a0d2ea4
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-06-17 22:43:08 -0400

    correção

---
+++ modulo-03-bootcamp-react/react-counter-01/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+import Counter from './components/Counter/Counter';
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Counter />
+        <Counter />
+        <Counter />
+      </Fragment>
+    );
+  }
+}
