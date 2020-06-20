5e3ca116573b96111fd169d23b9a34980f8b43e2
rodyokoo <39280721+rodyokoo@users.noreply.github.com>
2020-06-20 19:16:23 -0400

    trabalho pratico final

--- modulo-03-bootcamp-react/trabalho-pratico/src/App.js
+++ modulo-03-bootcamp-react/trabalho-pratico/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
+import { calculateSalaryFrom } from './components/helper/salary';
 
 export default class App extends Component {
   constructor() {
@@ -8,12 +9,39 @@ export default class App extends Component {
       salary: '',
     };
   }
+
+  handleChangeSalary = (newSalary) => {
+    this.setState({
+      salary: newSalary,
+    });
+    const table = calculateSalaryFrom(newSalary);
+    const inss = table.baseINSS;
+    const inssDes = table.discountINSS;
+    const irff = table.baseIRPF;
+    const irffDes = table.discountIRPF;
+    const salaryDes = table.netSalary;
+    this.setState({
+      inss,
+      inssDes,
+      irff,
+      irffDes,
+      salaryDes,
+    });
+  };
+
   render() {
+    const { salary, inss, inssDes, irff, irffDes, salaryDes } = this.state;
     return (
       <div>
-        <h2>React Salário</h2>
-
-        <Header />
+        <Header
+          salary={salary}
+          onChangeSalary={this.handleChangeSalary}
+          inss={inss}
+          inssDes={inssDes}
+          irff={irff}
+          irffDes={irffDes}
+          salaryDes={salaryDes}
+        />
       </div>
     );
   }
--- modulo-03-bootcamp-react/trabalho-pratico/src/components/header/Header.js
+++ modulo-03-bootcamp-react/trabalho-pratico/src/components/header/Header.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import css from './header.module.css';
 
 export default class Header extends Component {
+  handleInputChange = (event) => {
+    const newSalary = event.target.value;
+    this.props.onChangeSalary(newSalary);
+  };
   render() {
-    const { salary } = this.props;
+    const { salary, inss, inssDes, irff, irffDes, salaryDes } = this.props;
     return (
-      <div className="container">
-        <h3>Salário Bruto</h3>
-        <input type="number" value={salary} />
+      <div>
+        <div className={css.salario}>
+          <h2>React Salário</h2>
+          <h3>Salário Bruto</h3>
+          <input
+            type="number"
+            value={salary}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div className={css.result}>
+          <span>
+            Base INSS: R$
+            {inss}
+          </span>
+          <span>
+            Desconto INSS: R$ {inssDes} ({(inssDes / inss) * 100})%
+          </span>
+          <span>
+            Base IRFF: R$
+            {irff}
+          </span>
+          <span>
+            Desconto IRFF: R$
+            {irffDes}
+          </span>
+          <span>
+            Salário Líquido: R$ {salaryDes} ({(salaryDes / inss) * 100}%)
+          </span>
+        </div>
       </div>
     );
   }
